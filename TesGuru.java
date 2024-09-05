11758047d4247fb527a2daeaec00f64bc246d48c
Nero-Xrpl5 <sulungdinero@gmail.com>
2024-09-05 11:57:43 +0700

    Add files via upload

---
+++ TesGuru.java
@@ -0,0 +1,47 @@
+public class TesGuru {
+    public static void main(String[] args) {
+        //1. NIP
+        //2. NAMA
+        //3. MAPEL
+        //4. STATUS
+
+        // Kelas Guru
+
+        // Guru Rabani = new Guru(111, "Rabani", "Ekonomi", "Tetap");
+        // Rabani.print();
+        // System.out.println( );
+
+        // Guru Nafis = new Guru(112, "Nafis", "Agama", "Tetap");
+        // Nafis.print();
+        // System.out.println( );
+
+        // Guru Reyhan = new Guru(113, "Reyhan", "Pengembangan Game", "Tetap");
+        // Reyhan.print();
+        // System.out.println( );
+
+        // Guru Itoo = new Guru(114, "Itoo", "Filosofi", "Tetap");
+        // Itoo.print();
+        // System.out.println( );
+
+        // Guru Iqbal = new Guru(115, "Iqbal", "Informatika", "Shift");
+        // Iqbal.print();
+        // System.out.println( );
+
+
+        Guru pasha = new Guru(101, "Pasha", "RPL", "Tetap");
+        pasha.setNama("Bu Pasha");
+        pasha.setMapel("RPL");
+        pasha.setStatus("Tetap");
+        pasha.setNip(101);
+        pasha.print();
+        
+
+
+
+        
+    
+    }
+
+    
+    
+}
